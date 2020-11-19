2b1361e71663bc2f48f74fd81b315d9a709df0d4
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-11-19 15:36:56 +0400

    -- controller and service packages as well as classes are created.

---
+++ src/main/java/com/azerconnect/phonenumberschecker/controller/RestController.java
@@ -0,0 +1,5 @@
+package com.azerconnect.phonenumberschecker.controller;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+}
---
+++ src/main/java/com/azerconnect/phonenumberschecker/service/Service.java
@@ -0,0 +1,5 @@
+package com.azerconnect.phonenumberschecker.service;
+
+@org.springframework.stereotype.Service
+public class Service {
+}
