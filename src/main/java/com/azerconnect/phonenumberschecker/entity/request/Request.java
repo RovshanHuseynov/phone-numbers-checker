b1ba99e3c849d7339e1d076e3ccfeadcbb1b8c09
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-11-19 15:53:44 +0400

    -- request and response packages as well as classes are created.

---
+++ src/main/java/com/azerconnect/phonenumberschecker/entity/request/Request.java
@@ -0,0 +1,7 @@
+package com.azerconnect.phonenumberschecker.entity.request;
+
+public class Request {
+    private String[] msisdnList;
+    private String blacklistString;
+    private String whitelistString;
+}
---
+++ src/main/java/com/azerconnect/phonenumberschecker/entity/response/Response.java
@@ -0,0 +1,6 @@
+package com.azerconnect.phonenumberschecker.entity.response;
+
+public class Response {
+    private String number;
+    private String result;
+}
