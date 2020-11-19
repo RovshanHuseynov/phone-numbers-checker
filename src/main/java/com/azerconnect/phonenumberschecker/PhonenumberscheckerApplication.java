4c5bda29300a8699016433c524de8dfeeb030501
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-11-19 15:32:28 +0400

    -- IC

---
+++ src/main/java/com/azerconnect/phonenumberschecker/PhonenumberscheckerApplication.java
@@ -0,0 +1,15 @@
+package com.azerconnect.phonenumberschecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * http://localhost:8080/home
+ */
+
+@SpringBootApplication
+public class PhonenumberscheckerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PhonenumberscheckerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/azerconnect/phonenumberschecker/PhonenumberscheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azerconnect.phonenumberschecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonenumberscheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
