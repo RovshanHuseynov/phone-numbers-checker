be9f540f918d93f8f839d1207880f0d708d9ca12
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-11-22 01:23:36 +0400

    -- code structure is fixed. Duplications are sent to one method.

--- src/main/java/com/azerconnect/phonenumberschecker/service/Service.java
+++ src/main/java/com/azerconnect/phonenumberschecker/service/Service.java
@@ -10,14 +10,13 @@ import java.util.Map;
 public class Service {
     private InputList blackList;
     private InputList whiteList;
+    private int lenOfOneNumber;
 
     public Response IsEligibleToSell(Request request){
         Response response = new Response();
         blackList = new InputList();
         whiteList = new InputList();
-        boolean isBlackList;
-        boolean isWhiteList;
-        int lenOfOneNumber = request.getMsisdnList().get(0).length() - 3;    // do not have to consider 994 while checking
+        lenOfOneNumber = request.getMsisdnList().get(0).length() - 3;    // do not have to consider 994 while checking
 
         fillMapsAccordingToRequest("blackList", request.getBlacklistString());
 
@@ -58,92 +57,17 @@ public class Service {
         for(String currentPhoneNumber : request.getMsisdnList()){
             System.out.println(currentPhoneNumber + " is being checked");
             currentPhoneNumber = currentPhoneNumber.substring(3); // do not have to consider 994 while checking
-            isBlackList = false;
-            isWhiteList = request.getWhitelistString().length() == 0;
 
-            if(request.getBlacklistString().length() == 0 && isWhiteList){
+            if(request.getBlacklistString().length() == 0 && request.getWhitelistString().length() == 0){
                 response.getResponse().put("994" + currentPhoneNumber, "ok");
-                System.out.println("goooo");
-                continue;
             }
-
-            if(blackList.getExactMask().containsKey(currentPhoneNumber)){
+            else if(mapContains(currentPhoneNumber, blackList, "blackList")){
                 response.getResponse().put("994" + currentPhoneNumber, "msisdn = 994" + currentPhoneNumber + " is in blacklist");
-                System.out.println("blackList exact: true");
-                continue;
             }
-
-            Map<String, Boolean> temp = blackList.getRangeMask();
-            for(int i=1; i<lenOfOneNumber; i++){
-                if(temp.containsKey(currentPhoneNumber.substring(0, i))){
-                    //response.getResponse().put("994" + currentPhoneNumber, "msisdn = 994" + currentPhoneNumber + " is in blacklist");
-                    isBlackList = true;
-                    System.out.println("blackList range: " + isBlackList + " " + currentPhoneNumber.substring(0, i));
-                    break;
-                }
-            }
-
-            if(isBlackList){
-                response.getResponse().put("994" + currentPhoneNumber, "msisdn = 994" + currentPhoneNumber + " is in blacklist");
-                continue;
-            }
-
-            Map<String, String> temp1 = blackList.getWildcardMask();
-            String searchedKey, searchedValue;
-            for(int i=0; i<lenOfOneNumber; i++){
-                searchedKey = currentPhoneNumber.substring(0,i);
-                searchedValue = currentPhoneNumber.substring(i+1);
-                if(temp1.containsKey(searchedKey) && temp1.get(searchedKey).equals(searchedValue)){
-                    //response.getResponse().put("994" + currentPhoneNumber, "msisdn = 994" + currentPhoneNumber + " is in blacklist");
-                    isBlackList = true;
-                    System.out.println("blackList wildcard: true " + searchedKey + "_" + searchedValue);
-                    break;
-                }
-            }
-
-            if(isBlackList){
-                response.getResponse().put("994" + currentPhoneNumber, "msisdn = 994" + currentPhoneNumber + " is in blacklist");
-                continue;
-            }
-
-            if(whiteList.getExactMask().containsKey(currentPhoneNumber)){
+            else if(mapContains(currentPhoneNumber, whiteList, "whiteList")){
                 response.getResponse().put("994" + currentPhoneNumber, "ok");
-                System.out.println("whiteList exact: " + true);
-                continue;
             }
-
-            temp = whiteList.getRangeMask();
-            for(int i=1; i<lenOfOneNumber; i++){
-                if(temp.containsKey(currentPhoneNumber.substring(0, i))){
-                    //response.getResponse().put("994" + currentPhoneNumber, "ok");
-                    isWhiteList = true;
-                    System.out.println("whiteList range: " + isWhiteList + " " + currentPhoneNumber.substring(0, i));
-                    break;
-                }
-            }
-
-            if(isWhiteList){
-                response.getResponse().put("994" + currentPhoneNumber, "ok");
-                continue;
-            }
-
-            temp1 = whiteList.getWildcardMask();
-            for(int i=0; i<lenOfOneNumber; i++){
-                searchedKey = currentPhoneNumber.substring(0,i);
-                searchedValue = currentPhoneNumber.substring(i+1);
-                if(temp1.containsKey(searchedKey) && temp1.get(searchedKey).equals(searchedValue)){
-                    //response.getResponse().put("994" + currentPhoneNumber, "ok");
-                    isWhiteList = true;
-                    System.out.println("whiteList wildcard: " + isWhiteList + " " + searchedKey + "_" + searchedValue);
-                    break;
-                }
-            }
-
-            if(isWhiteList){
-                response.getResponse().put("994" + currentPhoneNumber, "ok");
-                continue;
-            }
-            else{
+            else {
                 System.out.println("not in whiteList");
                 response.getResponse().put("994" + currentPhoneNumber, "msisdn = 994" + currentPhoneNumber + " is not in whitelist");
             }
@@ -183,5 +107,34 @@ public class Service {
             }
             else map.getExactMask().put(currentNumber, true);
         }
+    }
+
+    private boolean mapContains(String currentPhoneNumber, InputList inputList, String nameOfInputList) {
+        Map<String, Boolean> map = inputList.getExactMask();
+        if(map.containsKey(currentPhoneNumber)){
+            System.out.println(nameOfInputList + " exact: true");
+            return true;
+        }
+
+        map = inputList.getRangeMask();
+        for(int i=1; i<lenOfOneNumber; i++){
+            if(map.containsKey(currentPhoneNumber.substring(0, i))){
+                System.out.println(nameOfInputList + " range: " + true + " " + currentPhoneNumber.substring(0, i));
+                return true;
+            }
+        }
+
+        Map<String, String> map2 = inputList.getWildcardMask();
+        String searchedKey, searchedValue;
+        for(int i=0; i<lenOfOneNumber; i++){
+            searchedKey = currentPhoneNumber.substring(0,i);
+            searchedValue = currentPhoneNumber.substring(i+1);
+            if(map2.containsKey(searchedKey) && map2.get(searchedKey).equals(searchedValue)){
+                System.out.println(nameOfInputList + " wildcard: true " + searchedKey + "_" + searchedValue);
+                return true;
+            }
+        }
+
+        return false;
     }
 }
